6cc634252103fb2034aabae4c79acf82549003ba
Jonatas Passos <jpc080496@gmail.com>
2022-07-11 21:56:50 -0300

    feat: implemented app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const loginRouter = require('../routes/login.route');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(loginRouter);
+
+app.use((err, _req, res, _next) => {
+  console.log(err);
+
+  return res.status(500).json({
+    error: 'internal server error',
+  });
+});
 
 module.exports = app;
