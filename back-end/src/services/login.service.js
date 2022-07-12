a8a74bdb079c3649d547b004e1a0825a29f84bbe
Jonatas Passos <jpc080496@gmail.com>
2022-07-11 22:00:25 -0300

    feat: implemented login service

---
+++ back-end/src/services/login.service.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+const { sign } = require('jsonwebtoken');
+const User = require('../database/models/User');
+
+const LoginService = async (email, password) => {
+  const user = await User.findOne({ where: { email } });
+
+  if (!user) {
+    return { error: 'User not found' };
+  }
+
+  const passwordConfirmed = crypto.createHash('md5').update(password).digest('hex');
+
+  if (passwordConfirmed !== user.password) {
+    return { error: 'User Unauthorized' };
+  }
+
+  const token = sign(
+    {},
+    process.env.APP_SECRET,
+    {
+      subject: user.id,
+      expiresIn: '1d',
+    },
+  );
+
+  return token;
+};
+
+module.exports = {
+  LoginService,
+};
