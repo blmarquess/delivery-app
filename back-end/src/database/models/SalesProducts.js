11941bf1670e40fbed13c4f9031dfdaef7f79c77
Jonatas Passos <jpc080496@gmail.com>
2022-07-12 16:31:40 -0300

    feat: create saleProduct model

---
+++ back-end/src/database/models/SalesProducts.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: { primaryKey: true, type: DataTypes.INTEGER },
+  sale_id: { type: DataTypes.INTEGER, foreignKey: true },
+  product_id: { type: DataTypes.INTEGER, foreignKey: true },
+  quantity: DataTypes.INTEGER,
+};
+
+module.exports = (sequelize) => {
+  const SalesProducts = sequelize.define("SalesProducts", Attributes, { modelName: 'salesProducts' });
+
+  SalesProducts.associate = (models) => {
+    SalesProducts.belongsToMany(models.Sales, { foreignKey: 'sale_id', as: 'sale' });
+  };
+
+  SalesProducts.associate = (models) => {
+    SalesProducts.belongsToMany(models.Products, { foreignKey: 'product_id', as: 'product' });
+  };
+
+  return SalesProducts;
+};
