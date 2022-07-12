feacf5a292680fb942c6110440270dbb06ba3377
Jonatas Passos <jpc080496@gmail.com>
2022-07-12 16:31:10 -0300

    feat: create product model

---
+++ back-end/src/database/models/Products.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: { primaryKey: true, type: DataTypes.INTEGER },
+  name: DataTypes.STRING,
+  price: DataTypes.DECIMAL,
+  url_image: DataTypes.STRING,
+};
+
+module.exports = (sequelize) => {
+  const Products = sequelize.define("Products", Attributes, { modelName: 'products' });
+
+  Products.associate = (models) => {
+    Products.hasToMany(models.SalesProducts, { foreignKey: 'product_id', as: 'sales_products' });
+  };
+
+  return Products;
+};
