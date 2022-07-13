3d59837769d3489de88cab65b4a6c3ff22f0bbfa
Jonatas Passos <jpc080496@gmail.com>
2022-07-13 16:19:29 -0300

    fix: change UUID to auto increment

--- back-end/src/database/seeders/20220713004822-users.js
+++ back-end/src/database/seeders/20220713004822-users.js
@@ -1,27 +1,25 @@
-const { randomUUID, createHash } = require('crypto');
-
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('users', [
       {
-        id: randomUUID(),
-        name: 'Jonatas Passos',
-        email: 'jonatas@gmail.com',
-        password: createHash('md5').update('jonatas').digest('hex'),
-        role: 'admin',
+        id: 1,
+        name: 'Delivery App Admin',
+        email: 'adm@deliveryapp.com',
+        password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+        role: 'administrator',
       },
       {
-        id: randomUUID(),
-        name: 'Luis Gustavo',
-        email: 'gustavo@gmail.com',
-        password: createHash('md5').update('gustavo').digest('hex'),
+        id: 2,
+        name: 'Fulana Pereira',
+        email: 'fulana@deliveryapp.com',
+        password: '3c28d2b0881bf46457a853e0b07531c6',
         role: 'seller',
       },
       {
-        id: randomUUID(),
-        name: 'Gabriel Augusto',
-        email: 'gabriel@gmail.com',
-        password: createHash('md5').update('gabriel').digest('hex'),
+        id: 3,
+        name: 'Cliente Zé Birita',
+        email: 'zebirita@email.com',
+        password: '1c37466c159755ce1fa181bd247cb925',
         role: 'customer',
       },
     ], { timestamp: false });
