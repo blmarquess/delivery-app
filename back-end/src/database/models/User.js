3a1137f2d1e29d5a7377e534c7897be8a1b7384d
Jonatas Passos <jpc080496@gmail.com>
2022-07-13 16:18:48 -0300

    refactor: change attributes in model user

--- back-end/src/database/models/User.js
+++ back-end/src/database/models/User.js
@@ -1,15 +1,33 @@
 const { DataTypes } = require('sequelize');
 
 const Attributes = {
-  id: DataTypes.PRIMARY,
-  name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-  role: DataTypes.STRING,
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+    require: false,
+    defaultValue: 'customer'
+  },
 };
 
 module.exports = (sequelize) => {
-  const Users = sequelize.define("Users", Attributes, { modelName: 'users', timestamp: false });
+  const Users = sequelize.define("Users", Attributes, { tableName: 'users', timestamp: false });
 
   return Users;
 };
