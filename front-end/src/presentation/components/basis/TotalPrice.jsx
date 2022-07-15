330d9ec8cced39f95af6cdc4a69dfbca82545cb7
Bruno Marques <blmarques.dev@gmail.com>
2022-07-15 06:52:08 -0300

    feat(total-price): change to styled in button total price

--- front-end/src/presentation/components/basis/TotalPrice.jsx
+++ front-end/src/presentation/components/basis/TotalPrice.jsx
@@ -1,43 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import './TotalPrice.css';
 
 export default function TotalPrice() {
-  return (
-    <div className="total-price">TotalPrice</div>
-  );
+  return <TotalPriceButton>TotalPrice</TotalPriceButton>;
 }
 
-// import styled from 'styled-components';
-
-// const TotalPrice = styled.button`align-self: center;
-//   background: ${({ bgcolor }) => bgcolor || '#036B52'};
-//   border: none;
-//   box-shadow: 0 0 0.3rem black;
-//   border-radius: 0.3rem;
-//   position: fixed;
-//   bottom: ${(props) => props.bottom || '5%'};
-//   color: ${({ textcolor }) => textcolor || 'white'};
-//   cursor: pointer;
-//   font-size: ${({ fontesize }) => fontesize || '22px'};
-//   font-weight: 600;
-//   height: 70px;
-//   left: ${(props) => props.left || '80%'};
-//   margin: ${(props) => props.msize || '0'};
-//   outline: none;
-//   padding: 0 1rem;
-//   position: ${(props) => props.position || 'fixed'};
-//   right: ${(props) => props.right || 'none'};
-//   text-align: center;
-//   top: ${(props) => props.top || 'none'};
-//   width: ${(props) => props.wsize || '300px'};
-
-//   &:disabled {
-//     opacity: 0.6;
-//   }
-
-//   &:focus {
-//     outline: '#036B52';
-//   }
-// `;
-
-// export default TotalPrice;
+export const TotalPriceButton = styled.button`align-self: center;
+  background: ${({ bgcolor }) => bgcolor || '#036B52'};
+  border: none;
+  box-shadow: 0 0 0.3rem black;
+  border-radius: 0.3rem;
+  position: fixed;
+  bottom: ${(props) => props.bottom || '5%'};
+  color: ${({ textcolor }) => textcolor || 'white'};
+  cursor: pointer;
+  font-size: ${({ fontesize }) => fontesize || '22px'};
+  font-weight: 600;
+  height: 70px;
+  left: ${(props) => props.left || '80%'};
+  margin: ${(props) => props.msize || '0'};
+  outline: none;
+  padding: 0 1rem;
+  position: ${(props) => props.position || 'fixed'};
+  right: ${(props) => props.right || 'none'};
+  text-align: center;
+  top: ${(props) => props.top || 'none'};
+  width: ${(props) => props.wsize || '300px'};
+`;
