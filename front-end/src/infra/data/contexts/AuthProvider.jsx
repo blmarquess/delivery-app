2dd9c2432374fe0a5dfef03f2c5405ee3c4268bd
Bruno Marques <blmarques.dev@gmail.com>
2022-07-15 15:03:38 -0300

    refactor(code-parttner): fix all code in unic code style

--- front-end/src/infra/data/contexts/AuthProvider.jsx
+++ front-end/src/infra/data/contexts/AuthProvider.jsx
@@ -1,23 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import AuthContext from './AuthContext';
-// import useHttp from '../../../main/hooks/useHttp';
 
 export default function AuthProvider({ children }) {
   const [userState, setUser] = useState(null);
-  // const dataApi = useHttp;
-
-  // useEffect(() => {
-  //   const validateUser = async () => {
-  //     const token = localStorage.getItem('authToken');
-  //     if (token) {
-  //       const userInfos = await dataApi.post('/validate', { token });
-  //       localStorage.setItem('authToken', userInfos.token);
-  //       return setUser((prevState) => ({ ...prevState, role: userInfos.data }));
-  //     }
-  //   };
-  //   validateUser();
-  // }, [dataApi]);
 
   const login = (user) => setUser((prevState) => ({ ...user, ...prevState }));
 
