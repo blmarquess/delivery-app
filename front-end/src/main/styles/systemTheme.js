ff879e2c562a51d6396103104d3a2a34b2f162bd
Bruno Marques <blmarques.dev@gmail.com>
2022-07-15 18:25:12 -0300

    feat(theme): created theme provider

---
+++ front-end/src/main/styles/systemTheme.js
@@ -0,0 +1,18 @@
+const systemTheme = {
+  background: '#E5E5E5',
+  color: {
+    primary: '#036B52',
+    secondary: '#421981',
+    tertiary: '#2FC18C',
+    quaternary: '#056CF9',
+    success: '#00CC9B',
+    warning: '#CCB800',
+    loading: '#66CC00',
+  },
+  text: {
+    light: '#F2FFFC',
+    dark: '#001813',
+  },
+};
+
+export default systemTheme;
