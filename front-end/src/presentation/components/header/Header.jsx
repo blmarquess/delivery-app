1549a7ef9e6b7bad505fe633a547d38ac946161e
Bruno Marques <blmarques.dev@gmail.com>
2022-07-19 16:05:22 -0300

    feat(data-test-id): include data-tests id na page products

--- front-end/src/presentation/components/header/Header.jsx
+++ front-end/src/presentation/components/header/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import RedirectToPath from '../../../main/useCases/redirectToPath';
 
 export default function Header() {
   return (
     <div className="div1">
       <li className="btn-products">
-        <button type="button" onClick={ RedirectToPath('/customer/products') }>
+        <button type="button" onClick={ console.log('/customer/products') }>
           Produtos
         </button>
       </li>
