84dce3ade07a38f0b66fff1c4e1ee5d31bacf605
Yuri <yurifranco18@live.com>
2022-07-20 19:33:30 -0300

    feat(pages, mock, cards): was made some major changes on orders, and detaisl orders pages

--- front-end/src/presentation/components/cards/OrderCard.jsx
+++ front-end/src/presentation/components/cards/OrderCard.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './OrderCard.css';
 
 export default function OrderCard() {
-  return (
-    <div className="card-order">
-      <div className="card-order-box">
-        <div className="order-id-box">
-          <div>Pedido</div>
-          <div>Id do pedido</div>
-        </div>
-        <div className="status preparando">Entregue</div>
-        <div className="data-n-price">
-          <div className="data">Data</div>
-          <div className="data">Preço</div>
-        </div>
-      </div>
+  function verifyIfIsSeller() {
+    return window.location.pathname.includes('/seller/orders');
+  }
+
+  function renderAddress() {
+    return (
       <div className="address">
         <p className="CEP">Endereço</p>
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <Link className="order-link" to="/customer/details">
+      <div className="card-order">
+        <div className="card-order-box">
+          <div className="order-id-box">
+            <div>Pedido</div>
+            <div>Id do pedido</div>
+          </div>
+          <div className="status entregue-card">Entregue</div>
+          <div className="data-n-price">
+            <div className="data">Data</div>
+            <div className="data">Preço</div>
+          </div>
+        </div>
+        {verifyIfIsSeller() ? renderAddress() : '' }
+      </div>
+    </Link>
   );
 }
--- front-end/src/presentation/pages/CustomerOrderDetails.jsx
+++ front-end/src/presentation/pages/CustomerOrderDetails.jsx
@@ -1,89 +1,96 @@
-import React, { useEffect } from 'react';
-// import Context from '../../infra/data/contexts/Context';
-import TotalPrice from '../components/basis/TotalPrice';
+import React, { useContext, useState, useEffect } from 'react';
+import Context from '../../infra/data/contexts/Context';
+import { getSellersNameDB } from '../../main/hooks/useHttp';
+import ButtonSD from '../components/basis/ButtonSD';
+// import TotalPrice from '../components/basis/TotalPrice';
 import HeaderCustomer from '../components/header/HeaderCustomer';
 import './styles/CustomerCheckout.css';
+import './styles/CustomerOrderDetails.css';
 
 export default function CustomerCheckout() {
-  // const { cart, removeCartItem } = useContext(Context);
-  // const [carProducts, setCarProducts] = useState([]);
-  // const [sellersNames, setSellersNames] = useState([]);
-
-  // useEffect(() => {
-  //   async function getSellersName() {
-  //     const sellers = await getSellersName();
-  //     setSellersNames(sellers);
-  //   }
-  //   getSellersName();
-  // }, []);
+  const { cart } = useContext(Context);
+  const [carProducts, setCarProducts] = useState([]);
 
   useEffect(() => {
-    setCarProducts(cart.productsInCar);
+    async function getSellersName() {
+      const sellers = await getSellersNameDB();
+      setSellersNames(sellers);
+    }
+    getSellersName();
   }, []);
 
-  function removeFromCar(id) {
-    return removeCartItem(id);
-  }
+  useEffect(() => {
+    const filteredProducts = cart.productsInCar.filter((product) => product.qtd !== 0);
+    setCarProducts(filteredProducts);
+  }, [cart.productsInCar]);
+
   return (
     <div className="checkout-page">
       <HeaderCustomer />
       <h1>Finalizar Pedido</h1>
       <div className="checkout">
+        <div className="header-details">
+          <h2 className="hearder-id">Pedido7219</h2>
+          <p className="hearder-seller">P Vend. Fulana de tal</p>
+          <h2 className="entregue">Pendente</h2>
+          <h2 className="hearder-date">13/13/2013</h2>
+          <button className="header-button" type="button">Marcar como entregue</button>
+        </div>
         <div className="list-titles">
           <h2 className="title-item">Item</h2>
           <h2 className="title-description">Descrição</h2>
           <h2 className="title-quantity">Quantidade</h2>
           <h2 className="title-unit-value">Valor-Unitário</h2>
           <h2 className="title-sub-total">Sub-total</h2>
-          <h2 className="title-remove">Remover</h2>
         </div>
         {
-          carProducts.map((product) => (
+          carProducts.map((product, i) => (
             <ul key={ product.name } className="products-list">
-              <li className="item">1</li>
-              <li className="description">{ product.name }</li>
-              <li className="quantity">asdasd</li>
-              <li className="unit-value">{ `R$ ${product.price}` }</li>
-              <li className="sub-total">asdddd</li>
-              <li className="remove-item">
-                <button
-                  type="button"
-                  onClick={ () => removeFromCar(product.id) }
-                >
-                  Remover
-                </button>
+              <li
+                className="item"
+                data-testid={
+                  `customer_checkout__element-order-table-item-number-${i}`
+                }
+              >
+                1
+              </li>
+              <li
+                className="description"
+                data-testid={ `customer_checkout__element-order-table-name-${i}` }
+              >
+                { product.name }
+              </li>
+              <li
+                className="quantity"
+                data-testid={ `customer_checkout__element-order-table-quantity-${i}` }
+              >
+                {product.qtd}
+              </li>
+              <li
+                className="unit-value"
+                data-testid={ `customer_checkout__element-order-table-unit-price-${i}` }
+              >
+                { `R$ ${product.price}` }
+              </li>
+              <li
+                className="sub-total"
+                data-testid={ `customer_checkout__element-order-table-sub-total-${i}` }
+              >
+                { `R$ ${product.subTotal.toFixed(2)}` }
               </li>
             </ul>
           ))
         }
+        <div>
+          <ButtonSD
+            psize="1.5rem 2rem"
+            radius="10px"
+            data-testid="customer_checkout__element-order-total-price"
+          >
+            {`Total: R$ ${cart.totalCarPrice.toFixed(2)}`}
+          </ButtonSD>
+        </div>
       </div>
-      <h1>Detalhes e Endereço para Entrega</h1>
-      <div className="address-details">
-        <label htmlFor="seller">
-          <h3>P. Vendedora Responsável</h3>
-          <select className="seller-select" name="" id="seller">
-            {/* {
-              sellersNames.map((name) => (
-                <option key={ name } value={ name }>{name}</option>
-              ))
-            } */}
-          </select>
-        </label>
-        <label htmlFor="address">
-          <h3>Endereço</h3>
-          <input
-            className="address-input"
-            type="text"
-            id="address"
-            placeholder="Rua Churosbangos Churosbagos"
-          />
-        </label>
-        <label htmlFor="number">
-          <h3>Número</h3>
-          <input className="input-number" type="text" id="number" />
-        </label>
-      </div>
-      <TotalPrice />
     </div>
   );
 }
