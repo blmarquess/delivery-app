7c9577b046bd225b7f15ebafefa629e3898cf59a
Jonatas Passos <jpc080496@gmail.com>
2022-07-20 09:45:55 -0300

    feat: create seeders in products

--- back-end/src/database/seeders/20220713004822-users.js
+++ back-end/src/database/seeders/20220713004822-users.js
@@ -22,7 +22,7 @@ module.exports = {
         password: '1c37466c159755ce1fa181bd247cb925',
         role: 'customer',
       },
-    ], { timestamp: false });
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
