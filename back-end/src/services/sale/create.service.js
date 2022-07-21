4df2850eaeb34692ee18f04c834013089e2751a4
Luis Gustavo <l.gustavomelo@hotmail.com>
2022-07-21 16:50:47 -0300

    refactor: service sales

--- back-end/src/services/sale/create.service.js
+++ back-end/src/services/sale/create.service.js
@@ -2,16 +2,6 @@ const { Sales, SalesProducts } = require('../../database/models');
 
 const CreateSaleService = async (
   { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, products }) => {
-  /* const customerExists = await Users.findByPk(userId);
-
-  if (!customerExists) return { message: 'Customer not found' };
-
-  const sellerExists = await Users.findByPk(sellerId);
-
-  if (!sellerExists) return { message: 'Seller not found' }; */
-
-  console.log('antes sale');
-
   const sale = await Sales.create({
     userId,
     sellerId,
@@ -19,8 +9,6 @@ const CreateSaleService = async (
     deliveryAddress,
     deliveryNumber,
   });
-
-  // console.log('apos sale', sale);
 
   await Promise.all(products.map(async ({ id, quantity }) => {
     await SalesProducts.create({
