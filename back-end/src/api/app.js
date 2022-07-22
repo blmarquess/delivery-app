8c57408ea9e9a15a55228c3ac5e0066603e39d23
Bruno Marques <blmarques.dev@gmail.com>
2022-07-21 22:07:29 -0300

    feat(images): created router for get all images

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const loginRouter = require('../routes/login.route');
-const productRouter = require('../routes/product.route');
-const saleRouter = require('../routes/sale.route');
-const userRouter = require('../routes/user.route');
+const routerApp = require('../routes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/login', loginRouter);
-app.use('/products', productRouter);
-app.use('/sales', saleRouter);
-app.use('/users', userRouter);
+app.use(routerApp);
+app.use(express.static('public'));
 
 app.use((err, _req, res, _next) => {
   console.log(err);
---
+++ back-end/src/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+
+const routerApp = Router();
+
+routerApp.use('/login', require('./login.route'));
+
+routerApp.use('/products', require('./product.route'));
+
+routerApp.use('/sales', require('./sale.route'));
+
+routerApp.use('/users', require('./user.route'));
+
+module.exports = routerApp;
