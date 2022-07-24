74077027a3e0bfb4091f523c1a69e8778917fbed
alex <alexmaia.etel@gmail.com>
2022-07-23 21:13:09 -0300

    refactor(formatdate-ordercard): cahnge the formatdate to validate if the parameter exists and change the data-testid at the Order Card component

--- front-end/src/main/useCases/formatDate.js
+++ front-end/src/main/useCases/formatDate.js
@@ -1,4 +1,7 @@
 export default function formatData(oldDateFormat) {
+  if (!oldDateFormat) {
+    return null;
+  }
   const arr = oldDateFormat.split('-');
   const day = arr[2].substr(0, 2);
   const month = arr[1];
