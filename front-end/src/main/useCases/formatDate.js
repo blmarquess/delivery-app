bcd883d4957fb6bea4d36ff6ba1ad26117787a3d
Bruno Marques <blmarques.dev@gmail.com>
2022-07-24 02:26:02 -0300

    fix(timestamp): refactore timestamp on frontend to convert Z to UTC

--- front-end/src/main/useCases/formatDate.js
+++ front-end/src/main/useCases/formatDate.js
@@ -2,9 +2,10 @@ export default function formatData(oldDateFormat) {
   if (!oldDateFormat) {
     return null;
   }
-  const arr = oldDateFormat.split('-');
-  const day = arr[2].substr(0, 2);
-  const month = arr[1];
-  const year = arr[0];
+  const dateInUTCFormat = new Date(oldDateFormat);
+  const sliceNumber = -2;
+  const day = dateInUTCFormat.getDate();
+  const month = (`00${dateInUTCFormat.getMonth() + 1}`).slice(sliceNumber);
+  const year = dateInUTCFormat.getFullYear();
   return `${day}/${month}/${year}`;
 }
